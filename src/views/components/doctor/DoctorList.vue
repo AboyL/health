13f3db3fd199f1f0b3908ed54597391e5a7dfa6a
L <1053801783@qq.com>
2018-04-26 20:27:14 +0800

    更新了项目结构

--- src/views/components/doctor/DoctorList.vue
+++ src/views/components/doctor/DoctorList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <doctor-item v-for="item of list"
+    <doctor-item v-for="item of doctorList"
                  :key="item.id"
                  :item="item">
     </doctor-item>
@@ -8,6 +8,8 @@
 </template>
 <script>
 import DoctorItem from './DoctorItem'
+import hospitalService from 'service/hospital.service.js'
+
 export default {
   name: 'DoctorList',
   components: {
@@ -15,6 +17,39 @@ export default {
   },
   props: {
     list: Array
+  },
+  data () {
+    return {
+      doctorList: []
+    }
+  },
+  computed: {
+    subject () {
+      return this.$store.state.subject
+    },
+    alreadyChooseSuject: function () {
+      return this.$store.state.subject.length >= 2
+    }
+  },
+  mounted () {
+    if (this.alreadyChooseSuject) {
+      hospitalService.getDoctors(this.$store.state.subject)
+        .then((res) => {
+          console.log(res)
+          this.doctorList = res
+        })
+    }
+  },
+  watch: {
+    subject () {
+      if (this.alreadyChooseSuject) {
+        hospitalService.getDoctors(this.$store.state.subject)
+          .then((res) => {
+            console.log(res)
+            this.doctorList = res
+          })
+      }
+    }
   }
 }
 </script>
--- src/views/tabs/components/tab-doctor.vue
+++ src/views/tabs/components/tab-doctor.vue
@@ -2,13 +2,12 @@
  * @Author: L
  * @Date: 2018-04-25 23:31:51
  * @Last Modified by: L
- * @Last Modified time: 2018-04-26 18:45:15
+ * @Last Modified time: 2018-04-26 20:04:25
  */
 <template>
   <div class="tab-doctor">
     <subject-cascader></subject-cascader>
-    <doctor-list v-if="type==='DoctorList'"
-                 :list="doctorList"></doctor-list>
+    <doctor-list v-if="type==='DoctorList'"></doctor-list>
     <empty v-if="type==='Empty'"></empty>
   </div>
 </template>
@@ -16,8 +15,6 @@
 import SubjectCascader from 'components/cascader/SubjectCascader.vue'
 import Empty from 'components/layout/Empty.vue'
 import DoctorList from 'components/doctor/DoctorList.vue'
-
-import hospitalService from 'service/hospital.service.js'
 
 export default {
   name: 'TabDoctor',
@@ -35,24 +32,9 @@ export default {
     alreadyChooseSuject: function () {
       return this.$store.state.subject.length >= 2
     },
-    subject () {
-      return this.$store.state.subject
-    },
     type: function () {
       console.log('type')
       return this.alreadyChooseSuject ? 'DoctorList' : 'Empty'
-    }
-  },
-  watch: {
-    subject () {
-      console.log('object')
-      if (this.alreadyChooseSuject) {
-        hospitalService.getDoctors(this.$store.state.subject)
-          .then((res) => {
-            console.log(res)
-            this.doctorList = res
-          })
-      }
     }
   }
 }
