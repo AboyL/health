7cdc5afb16170cba2aaa4577223d8b8f3bc73470
L <1053801783@qq.com>
2018-05-30 13:23:22 +0800

    bug

--- src/views/hospital/components/hospitalRegistrationSheet.vue
+++ src/views/hospital/components/hospitalRegistrationSheet.vue
@@ -2,7 +2,7 @@
  * @Author: L
  * @Date: 2018-05-07 11:35:43
  * @Last Modified by: L
- * @Last Modified time: 2018-05-14 23:14:02
+ * @Last Modified time: 2018-05-25 14:00:59
  */
 <template>
   <div class="wrapper">
@@ -126,7 +126,7 @@ export default {
       // 判断是否到了下午了
       let nowTime = (new Date()).getHours()
       let isAfternoon = (nowTime > 11)// 十一点以后就不能再进行预约了
-      let isNight = (nowTime > 17)// 下午五点以后就不能再进行预约了
+      let isNight = (nowTime > 19)// 下午7点以后就不能再进行预约了
       this.today.morningTitle = this.today.morning ? '点击预约' : '预约已满'
       if (isAfternoon) {
         this.today.morning = false
